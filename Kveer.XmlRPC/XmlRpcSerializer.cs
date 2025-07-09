7d020762c02d5e54f87f9de3d2fe0c954fec74a1
Fred Sobel <fred.sobel@gmail.com>
2025-07-08 20:13:33 -0500

    leave stream open after the reader is disposed

--- Kveer.XmlRPC/XmlRpcSerializer.cs
+++ Kveer.XmlRPC/XmlRpcSerializer.cs
@@ -205,13 +205,15 @@ namespace CookComputing.XmlRpc
 			}
 
 			var xdoc = new XmlDocument { PreserveWhitespace = true };
+			var stng = new XmlReaderSettings();
+			#if (!COMPACT_FRAMEWORK)
+			stng.DtdProcessing = DtdProcessing.Prohibit;
+			#endif
 			try
 			{
-				using (var xmlRdr = new XmlTextReader(stm))
+				using (var reader = new StreamReader(stm, Encoding.UTF8, true, 1024, leaveOpen: true))
+				using (var xmlRdr = XmlReader.Create(reader, stng))
 				{
-#if (!COMPACT_FRAMEWORK)
-					xmlRdr.DtdProcessing = DtdProcessing.Prohibit;
-#endif
 					xdoc.Load(xmlRdr);
 				}
 			}
